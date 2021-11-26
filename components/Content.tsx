b9603c61b0589657be295c17b6113d48580a69d3
Victor G. Rodrigues <victorgrodriguesm7@gmail.com>
2021-11-26 11:24:06 -0300

    feat: create Content

---
+++ components/Content.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import {
+    MainContainer,
+    Container,
+    Title,
+    Subtitle,
+    ImageContainer
+} from '../styles/components/content';
+import Button from './Button';
+
+export default function Content(){
+    return (
+        <MainContainer>
+            <Container>
+                <Title>More than just shorter links</Title>
+                <Subtitle>Build your brand&apos;s recognition and get detailed insights on how your links are performing.</Subtitle>
+                <Button 
+                    fontSize={"medium"}
+                    borderRadius={'3rem'}
+                    padding={"0.75rem 1.75rem"}
+                    margin={"1rem 0 0"}>
+                    Get Started
+                </Button>
+            </Container>
+            <ImageContainer>
+                <Image
+                    src="/assets/illustration-working.svg"
+                    alt="Working Illustration"
+                    layout="fill"
+                    />
+            </ImageContainer>
+        </MainContainer>
+    )
+}
---
+++ styles/components/content.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-content: center;
+    margin-top: 3rem;
+    padding: 0 0 5rem 10%;
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 2rem;
+`;
+
+export const Title = styled.h1`
+    font-size: ${props => props.theme.fontSize.xLarge};
+    font-weight: ${props => props.theme.fontWeight.bold};
+`;
+
+export const Subtitle = styled.p`
+    font-size: 1.1rem;
+    filter: opacity(0.5);
+`;
+
+export const ImageContainer = styled.div`
+    position: relative;
+    margin-left: 4rem;
+    height: 22rem;
+`;
