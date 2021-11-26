b8b2a6f8fa6fc5e57329645d4623e379a5377243
Victor G. Rodrigues <victorgrodriguesm7@gmail.com>
2021-11-26 11:19:33 -0300

    style: add root, theme and fontFamily to Globals

--- styles/global.ts
+++ styles/global.ts
@@ -5,15 +5,18 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
   }
 
-  html, body {
+  html, body, #__next {
     min-height: 100vh;
+    background: ${props => props.theme.colors.neutral.darkViolet}
   }
-
-  body {
+  
+  #__next {
     display: flex;
     flex-direction: column;
+    width: 100%;
   }
 `;
 
