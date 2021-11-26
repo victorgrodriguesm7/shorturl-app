08fb25855266d0c306100a6709b6c4790aa14262
Victor G. Rodrigues <victorgrodriguesm7@gmail.com>
2021-11-26 11:16:55 -0300

    feat: add style theme

---
+++ styles/theme.ts
@@ -0,0 +1,27 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+    colors: {
+        primary: {
+            cyan: '#29C7C7',
+            violet: '#3B3054'
+        },
+        secondary: '#F46262',
+        neutral: {
+            gray: '#BFBFBF',
+            grayishViolet: '#9E9AA7',
+            darkBlue: '#35323E',
+            darkViolet: '#232127'
+        }
+    },
+    fontSize: {
+        xLarge: '4.5rem',
+        large: '2.25rem',
+        medium: '1rem',
+        small: '0.75rem'
+    },
+    fontWeight: {
+        bold: 700,
+        default: 500
+    }
+}
