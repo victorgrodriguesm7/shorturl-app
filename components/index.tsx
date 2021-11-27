5f3b2e9997bcef58bcdb4d9624c54b7f61f016c6
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-11-27 11:48:02 -0300

    style: center component imports in one file

---
+++ components/index.tsx
@@ -0,0 +1,11 @@
+import Button from "./Button"
+import Content from "./Content"
+import Header from "./Header"
+import UrlShorter from "./UrlShorter"
+
+export {
+    Button,
+    Content,
+    Header,
+    UrlShorter
+}
