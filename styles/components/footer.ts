7fd9ad9d226dfecdaf1e9d12bc7798dae831c9ea
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-11-29 13:54:17 -0300

    style: create style to Footer Component

---
+++ styles/components/footer.ts
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const GridContainer = styled.footer`
+    display: grid;
+    grid-template-columns: 2fr repeat(3, 1fr) 2fr;
+    gap: 2rem;
+
+    width: 100%;
+    height: max-content;
+    padding: 2rem 10% 3rem;
+`;
+
+export const LogoContainer = styled.div`
+    position: relative;
+    height: 1.5rem;
+    width: 6rem;
+
+    svg path {
+        fill: white;
+    }
+`;
+
+export const TopicContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const SocialMediaContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`;
+
+export const TopicTitle = styled.h3`
+    padding: 0.25rem 0 0.5rem;
+    font-size: ${props => props.theme.fontSize.medium};
+
+    color: white;
+`;
+
+export const TopicItem = styled.span`
+    color: white;
+    
+    opacity: 0.6;
+    font-size: ${props => props.theme.fontSize.small};
+    padding: 0.25rem 0;
+    width: fit-content;
+
+    transition: color 0.2s ease;
+
+    cursor: pointer;
+    &:hover {
+        color: ${props => props.theme.colors.primary.cyan};
+    }
+`;
+
+export const SocialMediaIcon = styled.a`
+    padding: 0.25rem 0 0;
+    width: 1.5rem;
+    height: 1.5rem;
+
+    svg path {
+        transition: all ease 0.3s;
+    }
+
+    &:hover {
+        svg path {
+            fill: ${props => props.theme.colors.primary.cyan};
+        }
+    }
+`;
